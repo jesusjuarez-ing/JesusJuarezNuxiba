1eaad37e4033d9bce2c741d52e018868166b00dd
AngelJesus696 <angeldejesusrg03@hotmail.com>
2026-05-30 16:31:56 -0600

    Implementación de modelos de dominio

---
+++ ML/Area.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ML;
+
+public class Area
+{
+    [Key]
+    public int IDArea { get; set; }
+
+    public string AreaName { get; set; } = string.Empty;
+
+    public int StatusArea { get; set; }
+
+    public DateTime CreateDate { get; set; }
+}
